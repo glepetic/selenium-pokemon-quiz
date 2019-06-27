c1482accff16079cb4facf8e43192020ff7d258d
Gonzalo Lepetic <gonzalolepetic@hotmail.com>
2019-06-27 17:08:21 -0300

    Remove unnecessary stuff. Add os distinction for driver

--- src/test/java/selenium/pokemon/quiz/PokemonQuizTest.java
+++ src/test/java/selenium/pokemon/quiz/PokemonQuizTest.java
@@ -1,18 +1,16 @@
 package selenium.pokemon.quiz;
 
 import com.fasterxml.jackson.core.type.TypeReference;
-import org.junit.Assert;
-import selenium.pokemon.quiz.dtos.Pokemon;
-import selenium.pokemon.quiz.pages.PokemonQuizPage;
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
+import selenium.pokemon.quiz.dtos.Pokemon;
+import selenium.pokemon.quiz.pages.PokemonQuizPage;
 import selenium.pokemon.quiz.utils.FileHandler;
 import selenium.pokemon.quiz.utils.JsonHandler;
+import selenium.pokemon.quiz.utils.SeleniumUtils;
 import selenium.pokemon.quiz.utils.Sorter;
 
 import java.util.List;
@@ -20,13 +18,13 @@ import java.util.stream.Collectors;
 
 public class PokemonQuizTest {
 
+    private static final String pokemonQuizBaseUrl = "https://www.sporcle.com/games/";
     private static WebDriver driver;
     private static List<String> pokemonNames;
-    private Sorter sorter = new Sorter();
 
     @BeforeClass
     public static void openBrowser(){
-        initializeDriver();
+        driver = SeleniumUtils.buildDriver();
         List<Pokemon> pokemons = new JsonHandler().deserialize(
                 new FileHandler().getContent("pokemon.json", false),
                         new TypeReference<List<Pokemon>>(){});
@@ -35,21 +33,7 @@ public class PokemonQuizTest {
 
     @AfterClass
     public static void closeBrowser(){
-        driver.close();
-    }
-
-    private static void initializeDriver(){
-        System.setProperty("webdriver.chrome.driver",
-                System.getProperty("user.dir") + "/src/main/resources/drivers/chromedriver_linux64");
-
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("test-type", "--disable-impl-side-painting", "disable-infobars",
-                "--silent", "--lang=en-GB");
-        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
-        capabilities.setCapability(ChromeOptions.CAPABILITY, options);
-
-        driver = new ChromeDriver(capabilities);
-        driver.manage().window().maximize();
+        SeleniumUtils.closeDriver(driver);
     }
 
     private void completePokemonQuiz(String url, Integer initialId, Integer finalId) {
@@ -57,43 +41,44 @@ public class PokemonQuizTest {
         PokemonQuizPage pokemonQuizPage = new PokemonQuizPage(driver);
         pokemonQuizPage.start();
         List<String> genPokemonNames =
-                sorter.sortByNameLength(pokemonNames.subList(initialId - 1, finalId));
+                new Sorter().sortByNameLength(pokemonNames.subList(initialId - 1, finalId));
         pokemonQuizPage.setPokemons(genPokemonNames);
         Assert.assertEquals((Integer) 100, pokemonQuizPage.getResultPoints());
     }
 
     @Test
     public void testPoke(){
-        completePokemonQuiz("https://www.sporcle.com/games/g/pokemon",1,151);
+        completePokemonQuiz(pokemonQuizBaseUrl + "g/pokemon",1,151);
     }
 
     @Test
     public void testPokeGen2(){
-        completePokemonQuiz("https://www.sporcle.com/games/g/pokemongen2",152,251);
+        completePokemonQuiz(pokemonQuizBaseUrl + "g/pokemongen2",152,251);
     }
 
     @Test
     public void testPokeGen3(){
-        completePokemonQuiz("https://www.sporcle.com/games/g/pokemongen3",252,386);
+        completePokemonQuiz(pokemonQuizBaseUrl + "g/pokemongen3",252,386);
     }
 
     @Test
     public void testPokeGen4(){
-        completePokemonQuiz("https://www.sporcle.com/games/g/pokemongen4",387,493);
+        completePokemonQuiz(pokemonQuizBaseUrl + "g/pokemongen4",387,493);
     }
 
     @Test
     public void testPokeGen5(){
-        completePokemonQuiz("https://www.sporcle.com/games/BluePikmin/pokemongen5",494,649);
+        completePokemonQuiz(pokemonQuizBaseUrl + "BluePikmin/pokemongen5",494,649);
     }
 
     @Test
     public void testPokeGen6(){
-        completePokemonQuiz("https://www.sporcle.com/games/dertswa687o/pokemon-gen-vi",650,721);
+        completePokemonQuiz(pokemonQuizBaseUrl + "dertswa687o/pokemon-gen-vi",650,721);
     }
 
     @Test
     public void testPokeGen7(){
-        completePokemonQuiz("https://www.sporcle.com/games/dertswa687o/pokemon-gen-vii",722,802);
+        completePokemonQuiz(pokemonQuizBaseUrl + "dertswa687o/pokemon-gen-vii",722,802);
     }
+
 }
