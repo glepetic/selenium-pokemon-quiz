016f997de5d47cb857848140355a7489b259d242
GONZALO LEPETIC <gonzalo.lepetic@despegar.com>
2019-06-27 17:08:21 -0300

    Remove unnecessary stuff. Add os distinction for driver

--- src/main/java/selenium/pokemon/quiz/dtos/Name.java
+++ src/main/java/selenium/pokemon/quiz/dtos/Name.java
@@ -3,8 +3,6 @@ package selenium.pokemon.quiz.dtos;
 public class Name {
 
     private String english;
-    private String japanese;
-    private String chinese;
 
     public String getEnglish() {
         return english;
@@ -14,19 +12,4 @@ public class Name {
         this.english = english;
     }
 
-    public String getJapanese() {
-        return japanese;
-    }
-
-    public void setJapanese(String japanese) {
-        this.japanese = japanese;
-    }
-
-    public String getChinese() {
-        return chinese;
-    }
-
-    public void setChinese(String chinese) {
-        this.chinese = chinese;
-    }
 }
